2fb5cfd41f3a04bec52fe36cb67e3ac67c191cf9
gabriel Urquiza <gabrielrodrigues2223@hotmail.com>
2025-07-29 21:33:57 -0300

    Exercicios

---
+++ APP-ACTIONS-TESTE/cypress/e2e/appActionsTest.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+const { email, senha } = require('../fixtures/data.json')
+const { profilePage } = require("../support/pages/profile.page")
+
+describe('Teste de Autenticação', () => {
+
+  it('deve fazer o login com sucesso', () => {
+    cy.login(email, senha)
+    profilePage.customerName().should('have.text', 'Urquiza Gabriel')
+  })
+
+    it('deve acessar o carrinho de compras com sucesso', () => {
+    cy.get('style').click
+  })
+})
