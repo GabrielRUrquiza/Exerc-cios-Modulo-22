c2e8059650d4cd1607731866d80fb5f21eb25595
gabriel Urquiza <gabrielrodrigues2223@hotmail.com>
2025-07-30 22:14:33 -0300

    novo arquivo

--- APP-ACTIONS-TESTE/cypress/e2e/appActionsTest.cy.js
+++ APP-ACTIONS-TESTE/cypress/e2e/appActionsTest.cy.js
@@ -5,12 +5,9 @@ const { profilePage } = require("../support/pages/profile.page")
 
 describe('Teste de Autenticação', () => {
 
-  it('deve fazer o login com sucesso', () => {
+  it('deve fazer o login com sucesso e acessar o carrinho', () => {
     cy.login(email, senha)
     profilePage.customerName().should('have.text', 'Urquiza Gabriel')
-  })
-
-    it('deve acessar o carrinho de compras com sucesso', () => {
-    cy.get('style').click
+    cy.get('[style="background-color: rgb(242, 242, 242);"] > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > .r-1d5kdc7 > :nth-child(1) > :nth-child(1) > .r-13awgt0 > :nth-child(1) > .r-mh9cjk > .r-18u37iz > :nth-child(2) > .css-146c3p1').click()
   })
 })
