b22a7ad6db240e6c60d5a2caf63da4a44e3a88b6
gabriel Urquiza <gabrielrodrigues2223@hotmail.com>
2025-08-19 00:25:00 -0300

    novo arquivo

--- APP-ACTIONS-TESTE/cypress/e2e/appActionsTest.cy.js
+++ APP-ACTIONS-TESTE/cypress/e2e/appActionsTest.cy.js
@@ -8,6 +8,6 @@ describe('Teste de Autenticação', () => {
   it('deve fazer o login com sucesso e acessar o carrinho', () => {
     cy.login(email, senha)
     profilePage.customerName().should('have.text', 'Urquiza Gabriel')
-    cy.get('[style="background-color: rgb(242, 242, 242);"] > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > .r-1d5kdc7 > :nth-child(1) > :nth-child(1) > .r-13awgt0 > :nth-child(1) > .r-mh9cjk > .r-18u37iz > :nth-child(2) > .css-146c3p1').click()
+    cy.get('div[style*="Ionicons"]').parent().click()
   })
 })
